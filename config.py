cdd56aedba7e2fb1c118f74e8df83be889492a7c
ysquaregit <38803281+ysquaregit@users.noreply.github.com>
2018-12-31 13:47:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ a valid WWO API key for retrieving weather up to 14 days in the future. Get an
 WWO API key here: https://developer.worldweatheronline.com/api/
 """
 
-WWO_API_KEY = '<INSERT_WWWO_API_KEY_HERE>'
+WWO_API_KEY = '3aee8267e820494ea0561303183112'
 MAX_FORECAST_LEN = 13
 _DEFAULT_TEMP_UNIT = 'F'
 
